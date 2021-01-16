06e67ca45a67c65e13946a463c6a8606959076f9
Karmasuki <69068318+Karmasuki@users.noreply.github.com>
2021-01-15 21:58:31 -0600

    Create MyUI(BadIknow).lua

---
+++ MyUI(BadIknow).lua
@@ -0,0 +1,262 @@
+--[[
+Note: I know the angles are off I do not give a shit about your feelings towards it, shut up 
+Anyways I am off have fun fucktards
+
+ ▄▄▄▄▄▄▄ ▄▄   ▄▄ ▄▄▄▄▄▄▄ ▄▄▄   ▄    ▄▄   ▄▄ ▄▄▄▄▄▄▄ ▄▄   ▄▄ 
+█       █  █ █  █       █   █ █ █  █  █ █  █       █  █ █  █
+█    ▄▄▄█  █ █  █       █   █▄█ █  █  █▄█  █   ▄   █  █ █  █
+█   █▄▄▄█  █▄█  █     ▄▄█      ▄█  █       █  █ █  █  █▄█  █
+█    ▄▄▄█       █    █  █     █▄   █▄     ▄█  █▄█  █       █
+█   █   █       █    █▄▄█    ▄  █    █   █ █       █       █
+█▄▄▄█   █▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█▄▄▄█ █▄█    █▄▄▄█ █▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█
+
+--]]
+
+
+
+--// Locals \\--
+
+local Based = Instance.new('ScreenGui')
+local Loader = Instance.new('Frame')
+local UICorner = Instance.new('UICorner')
+local random = Instance.new('TextLabel')
+local Info = Instance.new('TextLabel')
+local StartButton = Instance.new('TextButton')
+local UICorner_2 = Instance.new('UICorner')
+local DaExplot = Instance.new('Frame')
+local Title = Instance.new('TextButton')
+local UICorner_3 = Instance.new('UICorner')
+local Option1 = Instance.new('TextButton')
+local UICorner_4 = Instance.new('UICorner')
+local Option4 = Instance.new('TextButton')
+local UICorner_5 = Instance.new('UICorner')
+local Option3 = Instance.new('TextButton')
+local UICorner_6 = Instance.new('UICorner')
+local Option2 = Instance.new('TextButton')
+local UICorner_7 = Instance.new('UICorner')
+local UICorner_8 = Instance.new('UICorner')
+local Credits = Instance.new('Frame')
+local UICorner_9 = Instance.new('UICorner')
+local Creator = Instance.new('TextLabel')
+local Info_2 = Instance.new('TextLabel')
+local Settings = Instance.new('TextButton')
+local UICorner_10 = Instance.new('UICorner')
+local Settings_2 = Instance.new('Frame')
+local ImageLabel = Instance.new('ImageLabel')
+local Destroy = Instance.new('TextButton')
+local UICorner_11 = Instance.new('UICorner')
+local Reload = Instance.new('TextButton')
+local UICorner_12 = Instance.new('UICorner')
+local UICorner_13 = Instance.new('UICorner')
+
+--// UI \\--
+
+Based.Name = 'Based'
+Based.Parent = game:GetService('CoreGui')
+
+Loader.Name = 'Loader'
+Loader.Parent = Based
+Loader.BackgroundColor3 = Color3.fromRGB(16, 16, 16)
+Loader.Position = UDim2.new(0.38837406, 0, 0.149892941, 0)
+Loader.Size = UDim2.new(0, 264, 0, 276)
+
+UICorner.CornerRadius = UDim.new(0.100000001, 0)
+UICorner.Parent = Loader
+
+random.Name = 'Random'
+random.Parent = Loader
+random.BackgroundColor3 = Color3.fromRGB(53, 2, 2)
+random.BackgroundTransparency = 1.000
+random.Position = UDim2.new(0.162878796, 0, 0.0724637657, 0)
+random.Size = UDim2.new(0, 179, 0, 50)
+random.Font = Enum.Font.TitilliumWeb
+random.Text = 'UI :Troll:'
+random.TextColor3 = Color3.fromRGB(255, 255, 255)
+random.TextSize = 25.000
+
+Info.Name = 'Info'
+Info.Parent = random
+Info.BackgroundColor3 = Color3.fromRGB(53, 2, 2)
+Info.BackgroundTransparency = 1.000
+Info.Position = UDim2.new(-0.00471897423, 0, 0.992463768, 0)
+Info.Size = UDim2.new(0, 179, 0, 50)
+Info.Font = Enum.Font.TitilliumWeb
+Info.Text = 'V1.0.0 Poop'
+Info.TextColor3 = Color3.fromRGB(255, 255, 255)
+Info.TextSize = 25.000
+
+StartButton.Name = 'StartButton'
+StartButton.Parent = Loader
+StartButton.BackgroundColor3 = Color3.fromRGB(84, 42, 127)
+StartButton.Position = UDim2.new(0.106060624, 0, 0.807971001, 0)
+StartButton.Size = UDim2.new(0, 209, 0, 44)
+StartButton.Font = Enum.Font.TitilliumWeb
+StartButton.Text = 'Start'
+StartButton.TextColor3 = Color3.fromRGB(255, 255, 255)
+StartButton.TextSize = 30.000
+
+UICorner_2.CornerRadius = UDim.new(0.200000003, 0)
+UICorner_2.Parent = StartButton
+
+DaExplot.Name = 'Da Explot'
+DaExplot.Parent = Based
+DaExplot.BackgroundColor3 = Color3.fromRGB(16, 16, 16)
+DaExplot.BorderColor3 = Color3.fromRGB(24, 24, 24)
+DaExplot.Position = UDim2.new(0.328559399, 0, 0.476445317, 0)
+DaExplot.Size = UDim2.new(0, 406, 0, 356)
+
+Title.Name = 'Title'
+Title.Parent = DaExplot
+Title.BackgroundColor3 = Color3.fromRGB(88, 88, 88)
+Title.Position = UDim2.new(0.0935960561, 0, 0.0505617969, 0)
+Title.Size = UDim2.new(0, 326, 0, 50)
+Title.Font = Enum.Font.TitilliumWeb
+Title.Text = 'Title Here'
+Title.TextColor3 = Color3.fromRGB(255, 255, 255)
+Title.TextSize = 24.000
+
+UICorner_3.CornerRadius = UDim.new(0.150000006, 0)
+UICorner_3.Parent = Title
+
+Option1.Name = 'Option 1'
+Option1.Parent = DaExplot
+Option1.BackgroundColor3 = Color3.fromRGB(84, 42, 127)
+Option1.Position = UDim2.new(0.0935960561, 0, 0.244382024, 0)
+Option1.Size = UDim2.new(0, 326, 0, 50)
+Option1.Font = Enum.Font.TitilliumWeb
+Option1.Text = 'OptionHere'
+Option1.TextColor3 = Color3.fromRGB(255, 255, 255)
+Option1.TextSize = 24.000
+
+UICorner_4.CornerRadius = UDim.new(0.150000006, 0)
+UICorner_4.Parent = Option1
+
+Option2.Name = 'Option2'
+Option2.Parent = DaExplot
+Option2.BackgroundColor3 = Color3.fromRGB(84, 42, 127)
+Option2.Position = UDim2.new(0.0935960561, 0, 0.429775268, 0)
+Option2.Size = UDim2.new(0, 326, 0, 50)
+Option2.Font = Enum.Font.TitilliumWeb
+Option2.Text = 'OptionHere'
+Option2.TextColor3 = Color3.fromRGB(255, 255, 255)
+Option2.TextSize = 24.000
+
+UICorner_7.CornerRadius = UDim.new(0.150000006, 0)
+UICorner_7.Parent = Option2
+
+Option3.Name = 'Option3'
+Option3.Parent = DaExplot
+Option3.BackgroundColor3 = Color3.fromRGB(84, 42, 127)
+Option3.Position = UDim2.new(0.0935960561, 0, 0.612359583, 0)
+Option3.Size = UDim2.new(0, 326, 0, 50)
+Option3.Font = Enum.Font.TitilliumWeb
+Option3.Text = 'OptionHere'
+Option3.TextColor3 = Color3.fromRGB(255, 255, 255)
+Option3.TextSize = 24.000
+
+UICorner_6.CornerRadius = UDim.new(0.150000006, 0)
+UICorner_6.Parent = Option3
+
+Option4.Name = 'Option 4'
+Option4.Parent = DaExplot
+Option4.BackgroundColor3 = Color3.fromRGB(84, 42, 127)
+Option4.Position = UDim2.new(0.0915369391, 0, 0.795617998, 0)
+Option4.Size = UDim2.new(0, 326, 0, 50)
+Option4.Font = Enum.Font.TitilliumWeb
+Option4.Text = 'OptionHere'
+Option4.TextColor3 = Color3.fromRGB(255, 255, 255)
+Option4.TextSize = 24.000
+
+UICorner_5.CornerRadius = UDim.new(0.150000006, 0)
+UICorner_5.Parent = Option4
+
+
+
+UICorner_8.CornerRadius = UDim.new(0.0799999982, 0)
+UICorner_8.Parent = DaExplot
+
+Credits.Name = 'Credits'
+Credits.Parent = Based
+Credits.BackgroundColor3 = Color3.fromRGB(16, 16, 16)
+Credits.Position = UDim2.new(0.669755757, 0, 0.149892941, 0)
+Credits.Size = UDim2.new(0, 264, 0, 276)
+
+UICorner_9.CornerRadius = UDim.new(0.100000001, 0)
+UICorner_9.Parent = Credits
+
+Creator.Name = 'Creator'
+Creator.Parent = Credits
+Creator.BackgroundColor3 = Color3.fromRGB(53, 2, 2)
+Creator.BackgroundTransparency = 1.000
+Creator.Position = UDim2.new(0.162878796, 0, 0.0724637657, 0)
+Creator.Size = UDim2.new(0, 179, 0, 50)
+Creator.Font = Enum.Font.TitilliumWeb
+Creator.Text = 'Made by !fishgang Karma#7760'
+Creator.TextColor3 = Color3.fromRGB(255, 255, 255)
+Creator.TextSize = 25.000
+
+Info_2.Name = 'Info'
+Info_2.Parent = Creator
+Info_2.BackgroundColor3 = Color3.fromRGB(53, 2, 2)
+Info_2.BackgroundTransparency = 1.000
+Info_2.Position = UDim2.new(-0.00471897423, 0, 0.992463768, 0)
+Info_2.Size = UDim2.new(0, 179, 0, 50)
+Info_2.Font = Enum.Font.TitilliumWeb
+Info_2.Text = 'Shoutout to Myself'
+Info_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+Info_2.TextSize = 25.000
+
+Settings.Name = 'Settings'
+Settings.Parent = Credits
+Settings.BackgroundColor3 = Color3.fromRGB(84, 42, 127)
+Settings.Position = UDim2.new(0.102272749, 0, 0.804347932, 0)
+Settings.Size = UDim2.new(0, 210, 0, 44)
+Settings.Font = Enum.Font.TitilliumWeb
+Settings.Text = 'Settings'
+Settings.TextColor3 = Color3.fromRGB(255, 255, 255)
+Settings.TextSize = 30.000
+
+UICorner_10.CornerRadius = UDim.new(0.200000003, 0)
+UICorner_10.Parent = Settings
+
+Settings_2.Name = 'Settings'
+Settings_2.Parent = Based
+Settings_2.BackgroundColor3 = Color3.fromRGB(16, 16, 16)
+Settings_2.BorderColor3 = Color3.fromRGB(24, 24, 24)
+Settings_2.Position = UDim2.new(0.105307519, 0, 0.149892941, 0)
+Settings_2.Size = UDim2.new(0, 264, 0, 276)
+
+ImageLabel.Parent = Settings_2
+ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel.Position = UDim2.new(0.106060609, 0, 0.0217391308, 0)
+ImageLabel.Size = UDim2.new(0, 210, 0, 134)
+ImageLabel.Image = 'http://www.roblox.com/asset/?id=6225137345'
+
+Destroy.Name = 'Destroy'
+Destroy.Parent = Settings_2
+Destroy.BackgroundColor3 = Color3.fromRGB(84, 42, 127)
+Destroy.Position = UDim2.new(0.106060594, 0, 0.800724626, 0)
+Destroy.Size = UDim2.new(0, 210, 0, 45)
+Destroy.Font = Enum.Font.TitilliumWeb
+Destroy.Text = 'Destroy'
+Destroy.TextColor3 = Color3.fromRGB(255, 255, 255)
+Destroy.TextSize = 30.000
+
+UICorner_11.CornerRadius = UDim.new(0.200000003, 0)
+UICorner_11.Parent = Destroy
+
+Reload.Name = 'Reload'
+Reload.Parent = Settings_2
+Reload.BackgroundColor3 = Color3.fromRGB(84, 42, 127)
+Reload.Position = UDim2.new(0.106060609, 0, 0.554347813, 0)
+Reload.Size = UDim2.new(0, 210, 0, 44)
+Reload.Font = Enum.Font.TitilliumWeb
+Reload.Text = 'Reload'
+Reload.TextColor3 = Color3.fromRGB(255, 255, 255)
+Reload.TextSize = 30.000
+
+UICorner_12.CornerRadius = UDim.new(0.200000003, 0)
+UICorner_12.Parent = Reload
+
+UICorner_13.CornerRadius = UDim.new(0.100000001, 0)
+UICorner_13.Parent = Settings_2
